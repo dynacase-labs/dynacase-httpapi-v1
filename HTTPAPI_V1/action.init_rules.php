53938459c229ac8121989a4e43dd9ff4e4f92b42
Éric Brison <eric.brison@anakeen.com>
2015-06-17 11:12:18 +0200

    fix(rules): sort ascending in http rules (refs #5563)

--- HTTPAPI_V1/action.init_rules.php
+++ HTTPAPI_V1/action.init_rules.php
@@ -1,29 +1,35 @@
 <?php
+/*
+ * @author Anakeen
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
+ * @package FDL
+ */
 
-function init_rules(Action &$action) {
-
+function init_rules(Action & $action)
+{
+    
     $usage = new ActionUsage($action);
-
+    
     $usage->setStrictMode(false);
     $usage->verify(true);
     $directoryIterator = new DirectoryIterator("./HTTPAPI_V1/rules.d/");
-    $jsonIterator = new RegexIterator($directoryIterator, "/.*\.json/");
+    $jsonIterator = new RegexIterator($directoryIterator, "/.*\.json$/");
     $jsonList = array();
-    foreach($jsonIterator as $currentFile) {
+    foreach ($jsonIterator as $currentFile) {
         $jsonList[] = $currentFile->getPathName();
     }
-    rsort($jsonList);
+    sort($jsonList);
     $rules = array();
-    foreach($jsonList as $currentPath) {
+    foreach ($jsonList as $currentPath) {
         $currentRule = file_get_contents($currentPath);
         $currentRule = json_decode($currentRule, true);
         if (json_last_error() !== JSON_ERROR_NONE) {
-            throw new Exception("Unable to read and decode ". $currentPath);
+            throw new Exception("Unable to read and decode " . $currentPath);
         }
         $rules = array_merge($rules, $currentRule);
     }
     $rules = json_encode($rules);
     ApplicationParameterManager::setParameterValue(ApplicationParameterManager::CURRENT_APPLICATION, "CRUD_CLASS", $rules);
-
+    
     Redirect($action, "HTTPAPI_V1", "DEFAULT_PAGE");
 }
