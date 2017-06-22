e846ada3cf9686e8309bb4a609845f059e5e4afd
Éric Brison <eric.brison@anakeen.com>
2017-06-22 17:44:07 +0200

    fix(auth): no special case (refs #6638)

--- HTTPAPI_V1/ContextManager.php
+++ HTTPAPI_V1/ContextManager.php
@@ -10,43 +10,31 @@ class ContextManager
      */
     public static function controlAuthent()
     {
-        $authtype = AuthenticatorManager::getAuthType();
+        // Ask authentification if HTML required
+        $urlInfo = parse_url($_SERVER["REQUEST_URI"]);
+        $headers = apache_request_headers();
+        $askAuthent = (preg_match("/\\.html$/", $urlInfo["path"]) || (!empty($headers["Accept"]) && preg_match("@\\btext/html\\b@", $headers["Accept"])));
         
-        if ($authtype == 'apache') {
-            // Apache has already handled the authentication
-            global $_SERVER;
-            if ($_SERVER['PHP_AUTH_USER'] == "") {
+        $status = AuthenticatorManager::checkAccess(null, !$askAuthent);
+        
+        switch ($status) {
+            case \Authenticator::AUTH_OK: // it'good, user is authentified
+                break;
+
+            default:
+                $auth = AuthenticatorManager::$auth;
+                if ($auth === false) {
+                    $exception = new \Dcp\HttpApi\V1\Api\Exception("Could not get authenticator");
+                    $exception->setHttpStatus("500", "Could not get authenticator");
+                    $exception->setUserMessage("Could not get authenticator");
+                    throw $exception;
+                }
+                
                 $exception = new \Dcp\HttpApi\V1\Api\Exception("User must be authenticated");
                 $exception->setHttpStatus("403", "Forbidden");
                 throw $exception;
-            }
-        } else {
-            // Ask authentification if HTML required
-            $urlInfo = parse_url($_SERVER["REQUEST_URI"]);
-            $headers = apache_request_headers();
-            $askAuthent = (preg_match("/\\.html$/", $urlInfo["path"]) || (!empty($headers["Accept"]) && preg_match("@\\btext/html\\b@", $headers["Accept"])));
-            
-            $status = AuthenticatorManager::checkAccess(null, !$askAuthent);
-            
-            switch ($status) {
-                case \Authenticator::AUTH_OK: // it'good, user is authentified
-                    break;
-
-                default:
-                    $auth = AuthenticatorManager::$auth;
-                    if ($auth === false) {
-                        $exception = new \Dcp\HttpApi\V1\Api\Exception("Could not get authenticator");
-                        $exception->setHttpStatus("500", "Could not get authenticator");
-                        $exception->setUserMessage("Could not get authenticator");
-                        throw $exception;
-                    }
-                    
-                    $exception = new \Dcp\HttpApi\V1\Api\Exception("User must be authenticated");
-                    $exception->setHttpStatus("403", "Forbidden");
-                    throw $exception;
-            }
-            $_SERVER['PHP_AUTH_USER'] = AuthenticatorManager::$auth->getAuthUser();
         }
+        $_SERVER['PHP_AUTH_USER'] = AuthenticatorManager::$auth->getAuthUser();
         // First control
         if (empty($_SERVER['PHP_AUTH_USER'])) {
             $exception = new \Dcp\HttpApi\V1\Api\Exception("User must be authenticated");
