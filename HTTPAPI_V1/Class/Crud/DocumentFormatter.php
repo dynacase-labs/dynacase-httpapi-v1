9d4d4f2d230fa9c5aefb5386f6126b112c5948b5
Éric Brison <eric.brison@anakeen.com>
2018-02-21 16:45:04 +0100

    fix(doc): Refactor to easily extends document crud (refs #7235)

--- HTTPAPI_V1/Class/Crud/DocumentFormatter.php
+++ HTTPAPI_V1/Class/Crud/DocumentFormatter.php
@@ -3,9 +3,11 @@
  * @author Anakeen
  * @package FDL
 */
+
 namespace Dcp\HttpApi\V1\Crud;
 
 use Dcp\HttpApi\V1\DocManager\Exception as DocumentException;
+
 /**
  * Class DocumentFormatter
  * This class is a facade of FormatCollection (had format for REST collection)
@@ -14,29 +16,31 @@ use Dcp\HttpApi\V1\DocManager\Exception as DocumentException;
  */
 class DocumentFormatter
 {
-    
-    static protected $uselessProperties = array(
-        "lockdomainid",
-        "domainid",
-        "svalues",
-        "ldapdn",
-        "comment",
-        "classname"
-    );
+
+    static protected $uselessProperties
+        = array(
+            "lockdomainid",
+            "domainid",
+            "svalues",
+            "ldapdn",
+            "comment",
+            "classname"
+        );
     /* @var \FormatCollection $formatCollection */
     protected $formatCollection;
-    protected $defaultProperties = array(
-        "initid",
-        "title",
-        "revision",
-        "state",
-        "icon",
-        "name"
-    );
+    protected $defaultProperties
+        = array(
+            "initid",
+            "title",
+            "revision",
+            "state",
+            "icon",
+            "name"
+        );
     protected $properties = array();
     protected $generateUrl = null;
     protected $rootPath;
-    
+
     public function __construct($source)
     {
         if (is_a($source, "Doc")) {
@@ -59,8 +63,7 @@ class DocumentFormatter
         }
         $this->rootPath = \Dcp\HttpApi\V1\Api\Router::getHttpApiParameter("REST_BASE_URL");
         /* init the standard generator of url (redirect to the documents collection */
-        $this->generateUrl = function ($document)
-        {
+        $this->generateUrl = function ($document) {
             if ($document) {
                 if ($document->defDoctype === "C") {
                     return URLUtils::generateURL(sprintf("families/%s.json", $document->name));
@@ -68,8 +71,9 @@ class DocumentFormatter
                     if ($document->doctype === "Z") {
                         return URLUtils::generateURL(sprintf("trash/%s.json", $document->initid));
                     } else {
-                        if ($document->locked == - 1) {
-                            return URLUtils::generateURL(sprintf("documents/%s/revisions/%d.json", $document->initid, $document->revision));
+                        if ($document->locked == -1) {
+                            return URLUtils::generateURL(sprintf("documents/%s/revisions/%d.json", $document->initid,
+                                $document->revision));
                         } else {
                             return URLUtils::generateURL(sprintf("documents/%s.json", $document->initid));
                         }
@@ -79,6 +83,7 @@ class DocumentFormatter
             return "";
         };
     }
+
     /**
      * Add a callable function that generate the document uri propertie
      * The callable take a Doc $document and return the uri
@@ -89,6 +94,7 @@ class DocumentFormatter
     {
         $this->generateUrl = $callable;
     }
+
     /**
      * add a set of attributes
      * Attributes that not part of the documents can be added
@@ -101,11 +107,13 @@ class DocumentFormatter
             $this->formatCollection->addAttribute($currentAttribute);
         }
     }
+
     /**
      * Add properties
      *
      * @param array $properties
-     * @param bool $withDefault (add the standard default list)
+     * @param bool  $withDefault (add the standard default list)
+     *
      * @throws Exception
      */
     public function setProperties(Array $properties, $withDefault = false)
@@ -118,6 +126,7 @@ class DocumentFormatter
             $this->properties = array_merge($this->properties, $this->defaultProperties);
         }
     }
+
     /**
      * Add a property
      *
@@ -142,6 +151,7 @@ class DocumentFormatter
             $this->properties[] = $propertyId;
         }
     }
+
     /**
      * Add the default properties
      */
@@ -149,6 +159,7 @@ class DocumentFormatter
     {
         $this->properties = $this->defaultProperties;
     }
+
     /**
      * Format the collection and return the array of result
      *
@@ -168,81 +179,101 @@ class DocumentFormatter
         $this->formatCollection->useShowEmptyOption = false;
         $this->formatCollection->setPropDateStyle(\DateAttributeValue::isoWTStyle);
         /** Format uniformly the void multiple values */
-        
-        $this->formatCollection->setAttributeRenderHook(function ($info, $attribute)
-        {
-            /**
-             * @var \NormalAttribute $attribute
-             */
-            if ($info === null) {
-                if ($attribute->isMultiple()) {
-                    $info = array();
-                } else {
-                    $info = new \StandardAttributeValue($attribute, null);
-                }
-            } elseif ($attribute->type === "docid" || $attribute->type === "account" || $attribute->type === "file" || $attribute->type === "image") {
-                
-                if (is_array($info)) {
-                    foreach ($info as & $oneInfo) {
-                        if (is_array($oneInfo)) {
-                            foreach ($oneInfo as & $subInfo) {
-                                if (!empty($subInfo->icon)) {
-                                    $this->rewriteImageUrl($subInfo->icon);
-                                }
-                            }
-                        } else {
-                            /**
-                             * @var \DocidAttributeValue|\ImageAttributeValue $oneInfo
-                             */
-                            if (!empty($oneInfo->icon)) {
-                                $this->rewriteImageUrl($oneInfo->icon);
-                            }
-                            
-                            if ($attribute->type === "image" && !empty($oneInfo->thumbnail)) {
-                                $this->rewriteThumbUrl($oneInfo->thumbnail);
-                            }
-                            if (($attribute->type === "image" || $attribute->type === "file") && !empty($oneInfo->url)) {
-                                $this->rewriteFileUrl($oneInfo->url);
-                            }
-                        }
-                    }
-                } else {
-                    if (!empty($info->icon)) {
-                        $this->rewriteImageUrl($info->icon);
-                    }
-                    if ($attribute->type === "image" && !empty($info->thumbnail)) {
-                        $this->rewriteThumbUrl($info->thumbnail);
-                    }
-                    if (($attribute->type === "image" || $attribute->type === "file") && !empty($info->url)) {
-                        $this->rewriteFileUrl($info->url);
-                    }
-                }
-            }
-            return $info;
+
+        $this->formatCollection->setAttributeRenderHook(function ($info, $attribute) {
+            return $this->attributeHook($info, $attribute);
         });
-        $generateUrl = $this->generateUrl;
         /** Add uri property and suppress state if no state **/
-        $this->formatCollection->setDocumentRenderHook(function ($values, \Doc $document) use ($generateUrl)
-        {
-            $values["uri"] = $generateUrl($document);
-            if (isset($values["properties"]["state"]) && !$values["properties"]["state"]->reference) {
-                unset($values["properties"]["state"]);
-            }
-            
-            if (isset($values["properties"]["icon"])) {
-                $this->rewriteImageUrl($values["properties"]["icon"]);
-            }
-            foreach ($values["properties"] as & $subProp) {
-                if (is_array($subProp) && !empty($subProp["icon"])) {
-                    $this->rewriteImageUrl($subProp["icon"]);
-                }
-            }
-            return $values;
+        $this->formatCollection->setDocumentRenderHook(function ($values, \Doc $document) {
+            return $this->documentHook($values, $document);
         });
-        
+
         return $this->formatCollection->render();
     }
-    
+
+    /**
+     * Hook applied for each documents data
+     * @param  array    $values
+     * @param \Doc $document
+     *
+     * @return mixed
+     */
+    protected function documentHook($values, \Doc $document)
+    {
+        $generateUrl = $this->generateUrl;
+        $values["uri"] = $generateUrl($document);
+        if (isset($values["properties"]["state"]) && !$values["properties"]["state"]->reference) {
+            unset($values["properties"]["state"]);
+        }
+
+        if (isset($values["properties"]["icon"])) {
+            $this->rewriteImageUrl($values["properties"]["icon"]);
+        }
+        foreach ($values["properties"] as & $subProp) {
+            if (is_array($subProp) && !empty($subProp["icon"])) {
+                $this->rewriteImageUrl($subProp["icon"]);
+            }
+        }
+        return $values;
+    }
+
+    /**
+     * Hook applied for each attribute data
+     * @param                  $info
+     * @param \NormalAttribute $attribute
+     *
+     * @return array|null|\StandardAttributeValue
+     */
+    protected function attributeHook($info, $attribute)
+    {
+        if ($info === null) {
+            if ($attribute->isMultiple()) {
+                $info = array();
+            } else {
+                $info = new \StandardAttributeValue($attribute, null);
+            }
+        } elseif ($attribute->type === "docid" || $attribute->type === "account" || $attribute->type === "file"
+            || $attribute->type === "image") {
+
+            if (is_array($info)) {
+                foreach ($info as & $oneInfo) {
+                    if (is_array($oneInfo)) {
+                        foreach ($oneInfo as & $subInfo) {
+                            if (!empty($subInfo->icon)) {
+                                $this->rewriteImageUrl($subInfo->icon);
+                            }
+                        }
+                    } else {
+                        /**
+                         * @var \DocidAttributeValue|\ImageAttributeValue $oneInfo
+                         */
+                        if (!empty($oneInfo->icon)) {
+                            $this->rewriteImageUrl($oneInfo->icon);
+                        }
+
+                        if ($attribute->type === "image" && !empty($oneInfo->thumbnail)) {
+                            $this->rewriteThumbUrl($oneInfo->thumbnail);
+                        }
+                        if (($attribute->type === "image" || $attribute->type === "file") && !empty($oneInfo->url)) {
+                            $this->rewriteFileUrl($oneInfo->url);
+                        }
+                    }
+                }
+            } else {
+                if (!empty($info->icon)) {
+                    $this->rewriteImageUrl($info->icon);
+                }
+                if ($attribute->type === "image" && !empty($info->thumbnail)) {
+                    $this->rewriteThumbUrl($info->thumbnail);
+                }
+                if (($attribute->type === "image" || $attribute->type === "file") && !empty($info->url)) {
+                    $this->rewriteFileUrl($info->url);
+                }
+            }
+        }
+        return $info;
+    }
+
     protected function rewriteImageUrl(&$imgUrl)
     {
         $pattern = "/resizeimg.php\\?img=(?:CORE%2F)?Images%2F([^&]+)&size=([0-9]+)/";
@@ -255,37 +286,42 @@ class DocumentFormatter
             $imgUrl = sprintf("%simages/recorded/sizes/%sx%sc/%s.png", $this->rootPath, $reg[2], $reg[2], $reg[1]);
         }
         //file/1383/0/icon/-1/200-0.jpg?inline=yes
-            $pattern = "%file/([0-9]+)/[0-9]+/([^/]+)/-1/([^\\?]+)\\?.*&width=([0-9]+)%";
-            if (preg_match($pattern, $imgUrl, $reg)) {
-                $imgUrl = sprintf("%sdocuments/%d/images/%s/-1/sizes/%sx%sc.png", $this->rootPath, $reg[1], $reg[2], $reg[4], $reg[4]);
-            }
-        }
-        protected function rewriteThumbUrl(&$imgUrl)
-        {
-            //http://localhost/tmp32/file/66519/0/en_photo/0/Migaloo-Baleine-04.jpg?cache=no&inline=yes&width=48&size=48&width=48
-            //file/66519/0/en_photo/4/faisan4.gif?cache=no&inline=yes&width=48
-            $pattern = "%file/(?P<docid>[0-9]+)/([^/]+)/(?P<attrid>[^/]+)/(?P<index>[^/]+)/.*&width=(?P<size>[0-9]+)%";
-            if (preg_match($pattern, $imgUrl, $reg)) {
-                $imgUrl = sprintf("%sdocuments/%d/images/%s/%s/sizes/%s.png", $this->rootPath, $reg["docid"], $reg["attrid"], $reg["index"], $reg["size"]);
-            }
-        }
-        
-        protected function rewriteFileUrl(&$fileUrl)
-        {
-            //file/66519/1461587595/en_photo/5/Agouti-Animals-Photos.JPG?cache=no&inline=yes
-            $pattern = "%file/(?P<docid>[0-9]+)/([^/]+)/(?P<attrid>[^/]+)/(?P<index>[^/]+)/(?P<filename>[^/?]+)%";
-            if (preg_match($pattern, $fileUrl, $reg)) {
-                $fileUrl = sprintf("%sdocuments/%d/files/%s/%s/%s", $this->rootPath, $reg["docid"], $reg["attrid"], $reg["index"], $reg["filename"]);
-            }
-        }
-        /**
-         * Return the format collection
-         *
-         * @return \FormatCollection
-         */
-        public function getFormatCollection()
-        {
-            return $this->formatCollection;
+        $pattern = "%file/([0-9]+)/[0-9]+/([^/]+)/-1/([^\\?]+)\\?.*&width=([0-9]+)%";
+        if (preg_match($pattern, $imgUrl, $reg)) {
+            $imgUrl = sprintf("%sdocuments/%d/images/%s/-1/sizes/%sx%sc.png", $this->rootPath, $reg[1], $reg[2],
+                $reg[4], $reg[4]);
         }
     }
+
+    protected function rewriteThumbUrl(&$imgUrl)
+    {
+        //http://localhost/tmp32/file/66519/0/en_photo/0/Migaloo-Baleine-04.jpg?cache=no&inline=yes&width=48&size=48&width=48
+        //file/66519/0/en_photo/4/faisan4.gif?cache=no&inline=yes&width=48
+        $pattern = "%file/(?P<docid>[0-9]+)/([^/]+)/(?P<attrid>[^/]+)/(?P<index>[^/]+)/.*&width=(?P<size>[0-9]+)%";
+        if (preg_match($pattern, $imgUrl, $reg)) {
+            $imgUrl = sprintf("%sdocuments/%d/images/%s/%s/sizes/%s.png", $this->rootPath, $reg["docid"],
+                $reg["attrid"], $reg["index"], $reg["size"]);
+        }
+    }
+
+    protected function rewriteFileUrl(&$fileUrl)
+    {
+        //file/66519/1461587595/en_photo/5/Agouti-Animals-Photos.JPG?cache=no&inline=yes
+        $pattern = "%file/(?P<docid>[0-9]+)/([^/]+)/(?P<attrid>[^/]+)/(?P<index>[^/]+)/(?P<filename>[^/?]+)%";
+        if (preg_match($pattern, $fileUrl, $reg)) {
+            $fileUrl = sprintf("%sdocuments/%d/files/%s/%s/%s", $this->rootPath, $reg["docid"], $reg["attrid"],
+                $reg["index"], $reg["filename"]);
+        }
+    }
+
+    /**
+     * Return the format collection
+     *
+     * @return \FormatCollection
+     */
+    public function getFormatCollection()
+    {
+        return $this->formatCollection;
+    }
+}
     
